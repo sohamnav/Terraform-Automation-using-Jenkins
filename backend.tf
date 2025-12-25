1b1f085845628ac7aa6f27b42b1a1b25f8b98a3f
Soham Navale <102356083+sohamnav@users.noreply.github.com>
2025-12-25 21:45:49 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "project-1-infrastructure-based-terraform-state"
+    bucket = "mydev-project-terraform-sample-batch-30-2025"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
+
 
